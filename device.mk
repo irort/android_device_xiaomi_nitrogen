06cdd7795a1a9c4357c6d4119e7c68fe7035d840
irort <irort3124@gmail.com>
2023-08-23 11:10:19 +0800

    nitrogen: Add overlay-lineage
    Signed-off-by: irort <irort3124@gmail.com>

--- device.mk
+++ device.mk
@@ -63,7 +63,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay
+    $(DEVICE_PATH)/overlay \
+    $(DEVICE_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
